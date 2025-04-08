6c4cdc7c594be3ec309a6319421ff6cd4e5293bd
Jangori-Dev <devopsaws496@gmail.com>
2025-04-08 21:45:10 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,4 +1,4 @@
-def download(repo)
+def download(string repo)
 {
   git "https://github.com/IntelliqDevops/${repo}"
 }
@@ -6,7 +6,11 @@ def build()
 {
   sh 'mvn package'
 }
-def deploy()
+def deploy(jobname,ip,context)
 {
-  sh 'scp /var/lib/jenkins/workspace/sharedlib'
+  sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:8080:/var/lib/tomcat10/webapps/${context}.war'
+}
+def testing(jobname)
+{
+  sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
 }
